df6363609950e14c0b6311fe2b1fe2fcaac59724
ANA CLARA DA CONCEICAO MACEDO DA SILVA <119021510@labin.canoas>
2017-09-23 15:24:00 -0300

    Commit 3 - 23/09/2017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit {
     selecionado: Vingador;
     novo: Vingador = new Vingador(0, '', ''); //utilizando o construtor
     // instanciar no cadastrarconfi
+    ultimo_id = 5;
+    editando = false;
 
     constructor() {
         this.title = 'Vingadores';
@@ -33,10 +35,48 @@ export class AppComponent implements OnInit {
     }
 
     cadastrar(): void {
-        const novoId: number = this.vingadores.length + 1;
-        this.vingadores.push(new Vingador(novoId, this.novo.nome, this.novo.pessoa));
-        
-        this.novo = new Vingador(0, '', '');
+        if(!this.editando){
+            const novoId: number = ++this.ultimo_id;
+            this.vingadores.push(new Vingador(novoId, this.novo.nome, this.novo.pessoa));        
+            this.novo = new Vingador(0, '', '');
+        }
+        else{
+            this.novo = new Vingador(0, '', '');
+            this.editando = false;
+        }
+    
         //funcionando com objetos
     }
+    encontrar(id: number): number{
+        //console.log("excluindo...", id);
+        let indice = -1;
+        for (let i=0; i< this.vingadores.length; i++){
+            if(this.vingadores[i].id == id){
+                indice = i;
+                //i= this.vingadores.length;
+                break;
+            }
+        }
+        return indice;
+    }
+    excluir(id: number): void{
+        //console.log("excluindo...", id);
+        const indice= this.encontrar(id);
+        if(indice != -1){
+            //delete this.vingadores[id];
+            this.vingadores.splice(indice, 1);
+            this.novo = new Vingador(0, '', '');
+        }
+        //this.vingadores.splice.indexOf(this.selecionado);
+    }
+
+    editar(id: number): void{
+        const indice = this.encontrar(id);
+
+        if(indice !== -1){
+            this.novo = this.vingadores[indice];
+            this.editando = true;
+        }
+    }
+    
 }
